75f5bba5e66f7329174ca8406663db441e932c22
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-06 11:29:01 -0500

    finished no stretch

--- client/src/App.js
+++ client/src/App.js
@@ -24,20 +24,20 @@ function App() {
   return (
     <div>
       <Router>
-        <nav className="container">
+        <nav className='container'>
           <h1>
-            React Plants <span role="img">🌿</span>
+            React Plants <span role='img'>🌿</span>
           </h1>
-          <ul className="steps">
+          <ul className='steps'>
             <li>
-              <NavLink exact to="/">
+              <NavLink exact to='/'>
                 Plants
               </NavLink>
             </li>
             <li>
-              <NavLink to="/cart">
+              <NavLink to='/cart'>
                 Cart
-                <span className="cart-badge">
+                <span className='cart-badge'>
                   {cart.length > 0 && cart.length}
                 </span>
               </NavLink>
@@ -46,11 +46,12 @@ function App() {
         </nav>
         <Route
           exact
-          path="/"
+          path='/'
           render={() => <PlantList addToCart={addToCart} />}
         />
         <Route
-          path="/cart"
+          exact
+          path='/cart'
           render={(props) => (
             <ShoppingCart
               {...props}
@@ -59,7 +60,7 @@ function App() {
             />
           )}
         />
-        <Route path="/checkout" component={CheckoutForm} />
+        <Route exact path='/checkout' component={CheckoutForm} />
       </Router>
     </div>
   );
